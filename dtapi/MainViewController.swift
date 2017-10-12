79df075a93a511284a223af7d5ddf5a2a99f37fd
Volodymyr <volodymyr@Volodymyrs-iMac.local>
2017-10-12 04:04:54 -0700

    first commit

---
+++ dtapi/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  dtapi
+//
+//  Created by Volodymyr on 10/10/17.
+//  Copyright © 2017 Volodymyr. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+
+}
--- dtapi/ViewController.swift
+++ dtapi/ViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let login = loginText.text!
+//        let password = passwordText.text!
+//        
+//        guard let url = URL(string: "http://vps9615.hyperhost.name") else { return }
+//        
+//        let session = URLSession.shared
+//        session.dataTask(with: url){ (data, response, error) in
+//            if let response = response {
+//                print(response)
+//            }
+//            if let date = data {
+//                print(data)
+//                do {
+//                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
+//                }
+//                catch{
+//                    print(error)
+//                }
+//            }
+//            
+//            }.resume()
+//        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        let requestBody = ["username": "admin", "password": "dtapi_admin"]
+        
+        guard let url = URL(string: "http://vps9615.hyperhost.name") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: requestBody, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                }
+                catch{
+                    print(error)
+                }
+            }
+        }.resume()
+    
     }
-
+    
+    
+//    guard let mainVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? MainViewController else  {
+    //        return
+    //        }
+    //        navigationController?.pushViewController(mainVC, animated: true)
 
 }
-

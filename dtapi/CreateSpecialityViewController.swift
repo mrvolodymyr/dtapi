4d3dc41725eb63514f2b1232b492b99bf35f19a0
Volodymyr <volodymyr@volodymyrs-imac.local>
2017-10-26 03:32:27 -0700

    26.10.17

--- dtapi/CreateSpecialityViewController.swift
+++ dtapi/CreateSpecialityViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class CreateSpecialityViewController: UIViewController {
     
-    @IBOutlet weak var specialytyCodeTaxtField: UITextField!
+    @IBOutlet weak var specialityCodeTextField: UITextField!
     @IBOutlet weak var specialityNameTextField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
+    
+    var speciality = SpecialityData.specialityData
+    var specialityInstance: SpecialityModel.Speciality? {
+        didSet {
+            self.view.layoutIfNeeded()
+            self.title = "Editing"
+            specialityNameTextField.text = specialityInstance?.speciality_name
+            specialityCodeTextField.text = specialityInstance?.speciality_code
+            createButton.titleLabel?.text = "Save"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
     
     @IBAction func CreateSpecialityButton(_ sender: Any) {
-        let specialityCodeText = specialytyCodeTaxtField.text!
-        let specialityNameText  = specialityNameTextField.text!
-        
-        HTTPService().createSpeciality(specialityCode: specialityCodeText, specialityName: specialityNameText)
+        HTTPService().createSpeciality(specialityCode: specialityCodeTextField.text!, specialityName: specialityNameTextField.text!)
         self.navigationController?.popViewController(animated: true)
     }
-    
+        
+
 }
--- dtapi/MainViewController.swift
+++ dtapi/MainViewController.swift
@@ -11,26 +11,27 @@ import UIKit
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var tableViewController: UITableView!
-    var specialityArray = SpecialityData.specialityData.specialityArray
+    
+    var specialities = SpecialityData.specialityData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    override func viewDidAppear(_ animated: Bool) {
         HTTPService().getSpeciality(){ (speciality: [SpecialityModel.Speciality]) in
-            self.specialityArray = speciality
-            self.tableViewController.reloadData()
+            SpecialityData.specialityData.specialityArray = speciality
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableViewController.reloadData()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return specialityArray.count
+        return specialities.specialityArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SpecialityTableViewCell
-        let sArray = self.specialityArray[indexPath.row]
+        let sArray = specialities.specialityArray[indexPath.row]
         cell.specialityIdLabel.text = sArray.speciality_id
         cell.specialityCodeLabel.text = sArray.speciality_code
         cell.specialityNameLabel.text = sArray.speciality_name
@@ -38,10 +39,10 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if indexPath.row < self.specialityArray.count {
-            HTTPService().deleteteSpeciality(id: specialityArray[indexPath.row].speciality_id, completionHandler: { (deleted) in
+        if indexPath.row < specialities.specialityArray.count {
+            HTTPService().deleteteSpeciality(id: specialities.specialityArray[indexPath.row].speciality_id, completionHandler: { (deleted) in
                 if deleted {
-                    self.specialityArray.remove(at: indexPath.row)
+                    self.specialities.specialityArray.remove(at: indexPath.row)
                     tableView.deleteRows(at: [indexPath], with: .top)
                     self.tableViewController.reloadData()
                 }
@@ -49,14 +50,19 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
             
         }
     }
-
+    
+    // editing
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let createSpecialityViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateSpecialityViewController") as? CreateSpecialityViewController else  { return }
+        createSpecialityViewController.specialityInstance = specialities.specialityArray[indexPath.row]
+        self.navigationController?.pushViewController(createSpecialityViewController, animated: true)
+    }
     
     @IBAction func createNewSpeciality(_ sender: Any) {
         guard let createSpecialityViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateSpecialityViewController") as? CreateSpecialityViewController else  { return }
         self.navigationController?.pushViewController(createSpecialityViewController, animated: true)
     }
-
-}
     
+}
 
 
